f014e363893ea933903759759ccd1e533afb142b
Ars003 <aditya003rs@gmail.com>
2026-01-29 20:38:22 +0530

    Adding get all airplanes api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -41,7 +41,7 @@ class CrudRepository{
 
   async getAll(){
     try {
-      const response = await this.model.findAll(data);
+      const response = await this.model.findAll();
       return response;
     } catch (error) {
       Logger.error('Something went wrong in the crud Repo : get');
